6ef84c2f9fa351a14de3b28ba278c0eecb5b1c25
chuquachula <33394101+chuquachula@users.noreply.github.com>
2018-06-25 15:30:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-1337;
-echo "ducnt";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+   height:100%;
+   width:100%;
+   background-image:url(http://0x1337.space/1m4g3s/404.jpg);
+   background-repeat:no-repeat;
+   background-size:cover;
+}
+</style>
+
