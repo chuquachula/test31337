a31f4681279d5744938448601ac817b149267efc
chuquachula <33394101+chuquachula@users.noreply.github.com>
2018-09-08 23:58:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
+echo "\nPoc subdomain takeover by ducnt\n";
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-   height:100%;
-   width:100%;
-   background-image:url(http://0x1337.space/1m4g3s/404.jpg);
-   background-repeat:no-repeat;
-   background-size:cover;
-}
-</style>
+
 
