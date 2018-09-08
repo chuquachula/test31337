0a71611a07f9159702cf1ee5f9300244e67c9f40
ducnt <nguyentheduc903@gmail.com>
2018-09-08 23:49:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
 <?php
+echo "\nPoC subdomain takeover by ducnt\n";
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-   height:100%;
-   width:100%;
-   background-image:url(http://0x1337.space/1m4g3s/404.jpg);
-   background-repeat:no-repeat;
-   background-size:cover;
-}
-</style>
 
